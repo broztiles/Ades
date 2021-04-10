d143ebb60a4a700334d3186fb8c003497ca2548f
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-10 13:32:09 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,14 @@
 let handler = function (m) {
+no = 0
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+ for (let jid of global.owner){
+ no += 1
+  this.sendContact(m.chat, jid, 'Owner '+no, m)
+}
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner|creator|ownerbot)$/i
 
 module.exports = handler
