b668319298a0aff3d01510c24d1c3ccd03e56a51
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-10 13:26:46 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Indosat Ooredoo [085642337380]
+│ • Telkomsel [081215199447]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
