c1a8342224e4443979317e4de7a7075566d68fb0
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-10 13:43:10 +0700

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,10 +1,17 @@
 const fetch = require('node-fetch')
 const FormData = require('form-data')
 const { MessageType } = require('@adiwajshing/baileys')
+const { sticker } = require('../lib/sticker')
+const imageToBase64 = require('image-to-base64')
 
 let handler  = async (m, { conn, text }) => {
-  if (text) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
-  else throw 'Uhm...Teksnya?'
+   pp = `https://api.vhtear.com/textxgif?text=${text}&apikey=Anakanjing123`
+    datae = await imageToBase64(JSON.stringify(pp).replace(/\"/gi, ''))
+   res = await Buffer.from(datae, 'base64')
+                     await sticker(res, false, 'Text Gif', 'Caliph Bot').then(gege => {
+                     conn.sendMessage(m.chat, gege, 'stickerMessage', { quoted: m })
+                     })
+  if (!text) throw 'Uhm...Teksnya?'
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
