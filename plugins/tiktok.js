24674ab204465f01dba28b0c0e2ba33c5c99a6c1
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-11 16:04:53 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,16 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/tiktok', {
-    url: args[0]
-  }, 'APIKEY'))
-  let json = await res.json()
-  if (!json.result) throw json
-  conn.sendFile(m.chat, json.result.url, 'tiktok.mp4', `
-_*Username:*_ @${json.result.username}
-`.trim(), m, false, {
-    thumbnail
-  })
+  try {
+  conn.sendFile(m.chat, `https://recoders-area.caliph.repl.co/api/tiktod/?url=${args[0]}&apikey=FreeApi`, 'tiktok', '', m, false)
+  } catch {
+  throw 'Maaf, Server Sedang Error'
+}
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
