635790bd72e8a1781b5cde362097bde4c07c8803
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-11 08:03:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281215199447'] // Put your number here
+global.owner = ['6281215199447','0'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
