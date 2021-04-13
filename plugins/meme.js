bcfde8bb836eab6287e975349220b3b64f2d0cfc
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-13 16:15:18 +0700

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-  let res = await fetch(`https://recoders-area.herokuapp.com/api/random/meme?apikey=FreeApi`)
+  let res = await fetch(`https://recoders-area.caliph.repl.co/api/random/meme?apikey=${APIKey["https://recoders-area.caliph.repl.co"]}`)
   let json = await res.json()
-  conn.sendFile(m.chat, json.result.url, `meme.${json.result.url.split('.')[1]}`, `${json.result.title}`, m, false)
+  conn.sendFile(m.chat, json.result.url, `meme`, `${json.result.title}`, m, false)
 }
 handler.help = ['meme']
 handler.tags = ['nulis']
