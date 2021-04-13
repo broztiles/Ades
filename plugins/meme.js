130b64499e2ced27b6c12caa078bf6ad0f3b5e89
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-13 16:37:10 +0700

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-  let res = await fetch(`https://recoders-area.caliph.repl.co/api/random/meme?apikey=${APIKey["https://recoders-area.caliph.repl.co"]}`)
+  let res = await fetch(`https://recoders-area.caliph.repl.co/api/random/meme?apikey=${APIKeys["https://recoders-area.caliph.repl.co"]}`)
   let json = await res.json()
   conn.sendFile(m.chat, json.result.url, `meme`, `${json.result.title}`, m, false)
 }
