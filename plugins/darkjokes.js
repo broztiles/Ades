d703be2343f48cf4100491504f956b93e0240cd5
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-05-12 16:36:59 +0700

    Update darkjokes.js

--- plugins/darkjokes.js
+++ plugins/darkjokes.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
   let res = await fetch(`https://recoders-area.caliph.repl.co/api/darkjokes?apikey=${APIKeys["https://recoders-area.caliph.repl.co"]}`)
-  let json = await res.json()
-  conn.sendFile(m.chat, json.result, 'darkjokes.png', `......`, m, false)
+  let json = await res.buffer()
+  conn.sendFile(m.chat, json, 'darkjokes.png', `......`, m, false)
 }
 handler.help = ['darkjokes']
 handler.tags = ['nulis']
