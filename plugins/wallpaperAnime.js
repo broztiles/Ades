36da93923369f16368cec5aad7702c16d76a5007
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-05-24 12:57:28 +0700

    Update wallpaperAnime.js

--- plugins/wallpaperAnime.js
+++ plugins/wallpaperAnime.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 await m.reply('Sedang Di Proses...')
    conn.sendFile(m.chat, 'https://recoders-area.caliph.repl.co/api/wpnime', 'wallpaper.jpeg', 
-'Wallpaper Nya Kak', msg)
+'Wallpaper Nya Kak', m)
 }
 handler.help = ['wallpaper', 'wp']
 handler.tags = ['internet']
