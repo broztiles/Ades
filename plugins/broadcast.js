150f8405289bb1f3cd27bab71d53d9ce89231e6c
broztiles <90729259+broztiles@users.noreply.github.com>
2021-09-24 10:54:14 -0500

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, text }) => {
   let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 All Chat Broadcast 」')
   for (let id of chats) conn.copyNForward(id, content, true)
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  conn.reply(m.chat, `_Enviar un mensaje de difusión a ${chats.length} chat_`, m)
 }
-handler.help = ['broadcast','bc'].map(v => v + ' <teks>')
+handler.help = ['broadcast','bc'].map(v => v + ' <texto>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)$/i
 handler.owner = true
