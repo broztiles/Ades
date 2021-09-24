d2c9c67344b3c2cb6181add1971836ef2f01e288
broztiles <90729259+broztiles@users.noreply.github.com>
2021-09-24 10:58:23 -0500

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -1,12 +1,12 @@
 let handler  = async (m, { conn, text }) => {
   let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
-  let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 All Group Broadcast 」')
+  let content = await conn.cMod(m.chat, m, /bc|anunciar/i.test(text) ? text : text + '\n' + readMore + '「 Anuncio a todos los grupos 」')
   for (let id of groups) conn.copyNForward(id, content, true)
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  conn.reply(m.chat, `_Enviar un mensaje de difusión a ${groups.length} grup_`, m)
 }
-handler.help = ['broadcastgroup','bcgc'].map(v => v + ' <teks>')
+handler.help = ['anunciar','bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcast|bc)(group|grup|gc)$/i
+handler.command = /^(anunciar|bc)(grupos|grup|gc)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
