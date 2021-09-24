afec22d440b7838e37f5b30bc15246d56d9959c3
broztiles <90729259+broztiles@users.noreply.github.com>
2021-09-24 10:42:54 -0500

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].isBanned = true
-    m.reply('Done!')
-  // } else m.reply('Ada nomor host disini...')
+    m.reply('Bot muerto en este grupo!')
+  // } else m.reply('Aquí hay un número de host...')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
