a7f6e820ec91666356e5423aa8205ebc71285b04
broztiles <90729259+broztiles@users.noreply.github.com>
2021-09-24 10:03:40 -0500

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -3,8 +3,8 @@ handler.before = m => {
   let user = global.DATABASE.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
-Selama ${clockString(new Date - user.afk)}
+Para AFK${user.afkReason ? ' después ' + user.afkReason : ''}
+Durante ${clockString(new Date - user.afk)}
 `.trim())
     user.afk = -1
     user.afkReason = ''
@@ -17,9 +17,9 @@ Selama ${clockString(new Date - user.afk)}
     if (!afkTime || afkTime < 0) continue
     let reason = user.afkReason || ''
     m.reply(`
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
-Selama ${clockString(new Date - afkTime)}
+¡No lo etiquetes!
+Él es AFK ${reason ? 'con razón ' + reason : 'tanpa alasan'}
+Durante ${clockString(new Date - afkTime)}
 `.trim())
   }
   return true
