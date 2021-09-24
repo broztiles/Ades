15b2c5686d117b3f4e71dd82ac962825cdf30440
broztiles <90729259+broztiles@users.noreply.github.com>
2021-09-24 10:24:08 -0500

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -3,13 +3,13 @@ let handler = async (m, { command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
     let which = command.replace(/add/i, '')
     if (!m.quoted) throw 'Reply Pesan!'
-    if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat list nya`
+    if (!text) throw `Usa *${usedPrefix}list${which}* para ver la lista`
     let msgs = global.DATABASE._data.msgs
-    if (text in msgs) throw `'${text}' telah terdaftar di list pesan`
+    if (text in msgs) throw `'${text}' Registrado en la lista de mensages`
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    m.reply(`Berhasil menambahkan pesan di list pesan sebagai '${text}'
+    m.reply(`Mensaje agregado exitosamente en la lista de mensajes como '${text}'
     
-Akses dengan ${usedPrefix}get${which} ${text}`)
+Accede con ${usedPrefix}get${which} ${text}`)
 }
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'add' + v + ' <text>')
 handler.tags = ['database']
