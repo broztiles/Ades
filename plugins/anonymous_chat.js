0b03ae0a02220c455aa4066d66bd10c87d6e1a3c
broztiles <90729259+broztiles@users.noreply.github.com>
2021-09-24 10:35:10 -0500

    Update anonymous_chat.js

--- plugins/anonymous_chat.js
+++ plugins/anonymous_chat.js
@@ -7,20 +7,20 @@ async function handler(m, { command }) {
         case 'next':
         case 'leave': {
             let room = Object.values(this.anonymous).find(room => [room.a, room.b].includes(m.sender))
-            if (!room) throw 'Kamu tidak sedang berada di anonymous chat'
+            if (!room) throw 'No estás en un chat anónimo'
             m.reply('Ok')
-            this.sendMessage([room.a, room.b].find(user => user !== m.sender), 'Partner meninggalkan chat😥', MessageType.text)
+            this.sendMessage([room.a, room.b].find(user => user !== m.sender), 'Los socios abandonan el chat😥', MessageType.text)
             delete this.anonymous[room.id]
             if (command === 'leave') break
         }
         case 'start': {
-            if (Object.values(this.anonymous).find(room => [room.a, room.b].includes(m.sender))) throw 'Kamu masih berada di dalam anonymous chat'
+            if (Object.values(this.anonymous).find(room => [room.a, room.b].includes(m.sender))) throw 'Sigues en el chat anónimo'
             let room = Object.values(this.anonymous).find(room => room.state === 'WAITING' && !([room.a, room.b].includes(m.sender)))
             if (room) {
                 this.sendMessage(room.a, 'partner Found!!\n\n/next : next Partner\n/leave : end chat', MessageType.text)
                 room.b = m.sender
                 room.state = 'CHATTING'
-                m.reply('partner Found!!\n\n/next : next Partner\n/leave : end chat')
+                m.reply('socio encontrado!!\n\n/siguiente: siguiente socio\n/dejar: finalizar chat')
             } else {
                 let id = + new Date
                 this.anonymous[id] = {
@@ -29,7 +29,7 @@ async function handler(m, { command }) {
                     b: '',
                     state: 'WAITING'
                 }
-                m.reply('Finding partner...')
+                m.reply('Encontrar socios...')
             }
             break
         }
